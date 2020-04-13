fdaf5e4554d8ed28c235c5e69ca46ff04784cfb5
Craighton <hancockcraighton@gmail.com>
2020-04-13 17:28:41 -0600

    add css, js, html

---
+++ js/site.js
@@ -0,0 +1,72 @@
+fetch('https://dog.ceo/api/breeds/list/all')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  setBreeds(data);
+  setBreedOptions();
+});
+
+let breeds = [];
+
+function setBreeds(data) {
+    breeds = data.message;
+}
+
+function setBreedOptions() {
+    Object.keys(breeds).forEach(breed => {
+        var breedOption = document.createElement("option");
+        breedOption.value = breed;
+        breedOption.appendChild(document.createTextNode(breed));
+        document.getElementById("dlBreeds").appendChild(breedOption);
+        if (breeds[breed].length > 0) {
+            breeds[breed].forEach(subBreed => {
+                var subBreedOption = document.createElement("option");
+                subBreedOption.value = breed + " - " + subBreed;
+                subBreedOption.appendChild(document.createTextNode(breed + " - " + subBreed));
+                document.getElementById("dlBreeds").appendChild(subBreedOption);
+            });
+        }
+    });
+}
+
+function searchBreeds() {
+    var imgContainer = document.getElementById("homepageImageContainer");
+    while(imgContainer.hasChildNodes()) {
+        imgContainer.removeChild(imgContainer.firstChild);
+    }
+    var selectedBreed = document.getElementById("inputSearchBreed").value;
+    var dashIndex = selectedBreed.indexOf("-");
+    if (dashIndex > 0) {
+        var breed = selectedBreed.substring(0, dashIndex - 1);
+        var subbreed = selectedBreed.substring(dashIndex + 2, selectedBreed.length);
+        fetch("https://dog.ceo/api/breed/" + breed + "/" + subbreed + "/images/random/20")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            showSearchResults(data.message);
+        });
+    } else {
+        fetch("https://dog.ceo/api/breed/" + selectedBreed + "/images/random/20")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            showSearchResults(data.message);
+        });
+    }
+}
+
+function showSearchResults(imageURLs) {
+    for(i = 0; i < imageURLs.length; i++) {
+        var url = imageURLs[i];
+        var img = document.createElement("img");
+        img.src = url;
+        img.classList = "image";
+        document.getElementById("homepageImageContainer").appendChild(img);
+    }
+}
